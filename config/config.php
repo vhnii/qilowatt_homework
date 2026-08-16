5153845bc8d05e509e8cb8acfc4b28ffcbf347a6
Rasmus Lääne <rasmus.laane@hotmail.com>
2026-08-16 21:18:05 +0300

    Added config file for API, timezone and price (fees, margin)

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Config;
+
+use Dotenv\Dotenv;
+
+Dotenv::createImmutable(__DIR__ . '/..')->safeLoad();
+
+return [
+    'api_base_url' => 'https://dashboard.elering.ee',
+    'region'       => 'ee',
+    'http_timeout' => 5,
+
+    'timezone' => 'Europe/Tallinn',    // Timezone to show in UI
+    'vat_rate'      => 0.24,    // 24% VAT
+    'network_fee'   => 0,   // s/kWh
+    'seller_margin' => 0,   // s/kWh
+    'default_window_hours' => 3,    // Default length of cheapest/most expensive window in hours (1-6)
+
+];
